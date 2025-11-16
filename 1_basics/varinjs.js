3a825bcde078eb7407352eea6b7bee28d10a05c2
Uttam148 <uttam.24scse1010001@galgotiasuniversity.ac.in>
2025-11-16 16:20:52 +0000

    container config done

---
+++ 1_basics/varinjs.js
@@ -0,0 +1,7 @@
+let name= "uttam" //best for defining variables
+const dob=2005 //fixed value
+var phoneNo=575654
+name2="priyanshu"
+let example
+name="gadha"
+console.table([name,dob,phoneNo,name2,example])
