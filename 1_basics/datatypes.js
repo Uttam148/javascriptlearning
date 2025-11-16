5dba17e2791169dde9f3fa06ddb714073925b0a0
Uttam148 <uttam.24scse1010001@galgotiasuniversity.ac.in>
2025-11-16 16:47:35 +0000

    datatypes

---
+++ 1_basics/datatypes.js
@@ -0,0 +1,7 @@
+let age =18 //int or number there is one more bigint
+let name ="inkipinki"//string
+let isThatAName=false//boolean
+let existence=null//standalone value
+let state;//undefined
+//symbol data type-unique
+console.log(typeof name) //tells type of data
