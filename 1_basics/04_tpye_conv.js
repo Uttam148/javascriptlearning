55b739dd323acf0363d29d28294192c28ec7b6c5
Uttam148 <uttam.24scse1010001@galgotiasuniversity.ac.in>
2025-11-16 17:10:25 +0000

    04_type_conv

---
+++ 1_basics/04_tpye_conv.js
@@ -0,0 +1,10 @@
+let num = "33"
+let realNum = Number(num)
+console.log(typeof num)
+console.log(typeof realNum)
+console.table(typeof [num,realNum]) //output - object always
+
+let num1="a34"
+let num2=Number(num1)
+console.log(typeof num2) // will be connverted to number
+console.log( num2) //NaN - not a number will  be  ouutput
