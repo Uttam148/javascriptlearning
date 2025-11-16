96e0303b70bf4f03b654ab23d165e930bd55f6f9
Uttam148 <uttam.24scse1010001@galgotiasuniversity.ac.in>
2025-11-16 17:22:23 +0000

    type conversion

--- 1_basics/04_tpye_conv.js
+++ 1_basics/04_tpye_conv.js
@@ -8,3 +8,5 @@ let num1="a34"
 let num2=Number(num1)
 console.log(typeof num2) // will be connverted to number
 console.log( num2) //NaN - not a number will  be  ouutput
+//true convertsto 1 and false to 0 in numbers
+//strings cant be convertd to numbers - Nan
