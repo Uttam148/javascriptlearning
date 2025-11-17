869675c0d34b410d7101969fd8d2bcee52863d9e
Uttam148 <uttam.24scse1010001@galgotiasuniversity.ac.in>
2025-11-17 12:35:48 +0000

    changes

---
+++ 1_basics/05_operations.js
@@ -0,0 +1,5 @@
+console.log(2**3) //this one was new to me it is power function -> 2^3
+
+// below ones are not that important
+console.log(+true)  //output: 1
+console.log(+"")   //  0
---
+++ 1_basics/06_strict_check.js
@@ -0,0 +1,1 @@
+console.log(2==="2") // it first check if data types are same of both nos/variables before comparing them//
